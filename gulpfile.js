019f6d62bcd80459dd340aa4abd88b3646a8c483
Julio César Palacios Rojas <juliopalro@gmail.com>
2018-03-20 01:31:44 -0500

    Inicializando y personalizando el proyecto para realizar un dashboard con boostrap 4

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var cssmin = require('gulp-cssmin');
+var watch = require('gulp-watch');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+
+gulp.task('build-css', function() {
+  return gulp.src('./node_modules/bootstrap/scss/bootstrap.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(cssmin())
+  .pipe(concat('bundle.css'))
+  .pipe(gulp.dest('./public/assets/css'));
+});
+gulp.task('build-js', function(cb) {
+  pump([
+    gulp.src([
+      './node_modules/jquery/dist/jquery.slim.min.js',
+      './node_modules/popper.js/dist/umd/popper.min.js',
+      './node_modules/bootstrap/dist/js/bootstrap.js',
+      './src/js/fontawesome-all.js'
+      ]), uglify(), concat('bundle.js'),
+    gulp.dest('./public/assets/js')
+    ], cb);
+});
+
+gulp.task('app-sass', function() {
+  return gulp.src('./src/sass/app.sass')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./public/assets/css'));
+});
+gulp.task('app-sass:watch', function () {
+    gulp.watch(['./src/sass/**/*.sass'], ['app-sass']);
+});
+
+gulp.task('app-js', function(cb) {
+  pump([
+    gulp.src('./src/js/app.js'),
+    // uglify(),
+    gulp.dest('./public/assets/js')
+    ], cb);
+});
+gulp.task('app-js:watch', function () {
+    gulp.watch(['./src/js/app.js'], ['app-js']);
+});
+
+gulp.task('build-html', function() {
+  return gulp.src('./src/html/*.html')
+    .pipe(gulp.dest('./public'));
+});
+gulp.task('build-html:watch', function () {
+    gulp.watch(['./src/html/*.html'], ['build-html']);
+});
+
+
+gulp.task('build', ['build-html', 'build-js', 'build-css', 'app-sass', 'app-js']);
