fa358ac762484c39c054aba38b4786b23f80b382
Juliopalro <juliopalro@gmail.com>
2018-03-31 01:13:09 -0500

    Agregando los estilos y el script para el collapse de los menús

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,15 @@
 /*Script for gadmin*/
+
+// $('[data-toggle="collapse-content"]').click(function(event) {
+// 	event.preventDefault();
+// 	$(this).parent().toggleClass('hidden-content');
+//   $(this).toggleClass('rotate-180');
+// });
+
+$( "#gadmin" ).on( "click", "button.navLeftToggler", function(event) {
+  event.preventDefault();
+  $("#leftPanel").toggleClass('leftPanelIco');
+  $("#rightPanel").toggleClass('rightPanelIco');
+  $(this).toggleClass('active');
+
+});
